4cdec8c9ed1ac486a4b160a6f23e40b2dad248bf
Willian Antunes <willian.lima.antunes@gmail.com>
2019-10-19 14:28:13 -0300

    Needed because of putExtra

--- app/src/main/java/br/com/willianantunes/androidplayground/services/persistence/domains.kt
+++ app/src/main/java/br/com/willianantunes/androidplayground/services/persistence/domains.kt
@@ -1,5 +1,6 @@
 package br.com.willianantunes.androidplayground.services.persistence
 
+import java.io.Serializable
 import java.time.LocalDate
 
-data class Person(val id: String?, val name: String, val birthday: LocalDate)
+data class Person(val id: String?, val name: String, val birthday: LocalDate) : Serializable
